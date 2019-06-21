c0bf07551db0e3d318aa2bf6f574484ed9e08696
Chaitanya <chaitanya@alphaclinicalsystems.com>
2019-06-21 14:30:51 +0530

    added @Entity to User

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,6 +1,12 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	  @Id
+	  private Long id;
 	  private String gender;
 	  private String name;
 	  
